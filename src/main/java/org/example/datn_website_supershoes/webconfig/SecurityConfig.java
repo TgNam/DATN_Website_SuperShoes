5ac6c4aa86f672f923d5c4ffb86926f56c0334e1
TgNam <nam060720041220@gmail.com>
2024-09-12 15:59:40 +0700

    support

--- src/main/java/org/example/datn_website_supershoes/webconfig/SecurityConfig.java
+++ src/main/java/org/example/datn_website_supershoes/webconfig/SecurityConfig.java
@@ -1,32 +1,58 @@
 package org.example.datn_website_supershoes.webconfig;
 
-import org.example.datn_website_supershoes.service.PasswordEncoderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Autowired
+    CustomPreFilter preFilter;
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
     @Bean
     public UserDetailsService userDetailsService(){
-        PasswordEncoder passwordEncoder = passwordEncoder();
-        UserDetails userDetails = User.builder()
-                .username("user")
-                .password(passwordEncoder.encode("user"))
-                .build();
-        return new InMemoryUserDetailsManager(userDetails);
+        return new CustomUserDetailsService();
+    }
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder());
+        provider.setUserDetailsService(userDetailsService());
+        return  provider;
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    public String[] PRIVATE_URL = { "/api/auth/**"};
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers(PRIVATE_URL).authenticated()
+                        .anyRequest().permitAll()
+                );
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.authenticationProvider(authenticationProvider());
+        http.addFilterBefore(preFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
     }
 }
