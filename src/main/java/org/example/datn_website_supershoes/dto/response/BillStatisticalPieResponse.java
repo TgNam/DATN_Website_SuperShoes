778f8709bdf082de4454ed8dc46247d86a3a4bba
Sleenma <quangd368@gmail.com>
2024-11-12 06:33:38 +0700

    fill du lieu thong ke

---
+++ src/main/java/org/example/datn_website_supershoes/dto/response/BillStatisticalPieResponse.java
@@ -0,0 +1,23 @@
+package org.example.datn_website_supershoes.dto.response;
+
+
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+public class BillStatisticalPieResponse {
+
+    private Date createdAt;
+
+    private int numberBill;
+
+    private BigDecimal price;
+
+}
--- src/main/java/org/example/datn_website_supershoes/dto/response/ProductViewCustomerReponse.java
+++ src/main/java/org/example/datn_website_supershoes/dto/response/ProductViewCustomerReponse.java
@@ -6,7 +6,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.math.BigDecimal;
-import java.util.Date;
 
 @Getter
 @Setter
